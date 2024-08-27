329f65e42a9fd731abfedea096bb7a913662e690
Blaxtron <abdellaahmed15@yahoo.com>
2024-08-28 00:17:01 +0300

    added a /health topic for each turtle

--- master/scripts/health_handler.py
+++ master/scripts/health_handler.py
@@ -6,6 +6,7 @@ class Turtle:
     def __init__(self,name,initial_health=100):
         self.name = name
         self.health = initial_health
+        self.publisher = rospy.Publisher(f"{self.name}/health", Int64, queue_size=10)
 
     def print_health(self):
         rospy.loginfo(f"{self.name}'s health={self.health}")
@@ -14,18 +15,17 @@ class Turtle:
 
 def main():
     rospy.init_node("health_manager",anonymous=False)
-    healthPublisher = rospy.Publisher("health_node",Int64,queue_size=10)
     r = rospy.Rate(5)
+    #assigns each turtle as a class objects
     turtle1 = Turtle("turtle1",100)
     turtle2 = Turtle("turtle2",100)
     turtle3 = Turtle("turtle3",100)
     turtle4 = Turtle("turtle4",100)
     turtleList = [turtle1,turtle2,turtle3,turtle4]
     while not rospy.is_shutdown():
-        for i in range(4):
-            if turtleList[i].health < 0:
-                turtleList[i].health = 0
-            healthPublisher.publish(turtleList[i].health)
+        #publishes each turtle's health to /TURTLENAME/health topic
+        for i in range(4):       
+            turtleList[i].publisher.publish(turtleList[i].health)
             turtleList[i].print_health()
             #turtleList[i].health-=1 #demonstration of health decreasing
         
